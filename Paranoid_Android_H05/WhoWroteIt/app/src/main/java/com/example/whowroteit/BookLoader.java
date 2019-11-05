c84854c4209626d36a432a295a451e707b0027f5
krishnasai6931 <krishnasai.6931@gmail.com>
2019-11-05 22:08:37 +0100

    Homework 5

---
+++ Paranoid_Android_H05/WhoWroteIt/app/src/main/java/com/example/whowroteit/BookLoader.java
@@ -0,0 +1,30 @@
+package com.example.whowroteit;
+
+import android.content.Context;
+import androidx.loader.content.AsyncTaskLoader;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    public BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
+
